f329880e7a011541aa11c03ff0e664b264c3bb70
Frank van Bakel <fvbakel@gmail.com>
2021-10-12 14:33:50 +0200

    Refactor lib names to remove extra lib name

---
+++ imgsolver/InputParser.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <vector>
+#include <assert.h>
+#include <algorithm>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/types_c.h>
+
+#include <solvercore/Nonogram.h>
+
+class InputParser{
+    public:
+        InputParser (int &argc, char **argv){
+            for (int i=1; i < argc; ++i)
+                this->tokens.push_back(std::string(argv[i]));
+        }
+
+        const std::string& getCmdOption(const std::string &option) const{
+            std::vector<std::string>::const_iterator itr;
+            itr =  std::find(this->tokens.begin(), this->tokens.end(), option);
+            if (itr != this->tokens.end() && ++itr != this->tokens.end()){
+                return *itr;
+            }
+            static const std::string empty_string("");
+            return empty_string;
+        }
+
+        bool cmdOptionExists(const std::string &option) const{
+            return std::find(this->tokens.begin(), this->tokens.end(), option)
+                   != this->tokens.end();
+        }
+    private:
+        std::vector <std::string> tokens;
+};
